9da5f3b8e783acefdad0072ee61ce7b317aa1c77
Yu-Chen Xue <32350820+SeanSyue@users.noreply.github.com>
2018-07-27 18:11:57 +0800

    Update setup.py
    menu and menu-gui are ready

--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ setup(
     author='seansyue',
     description='A prototype of custom ordering bot with smile activation',
 
-    packages=['', 'menu', 'CamUtils'],
+    packages=['', 'menu'],
     package_data={
-        'menu': ['rice_menu.txt', 'noodle_menu.txt', 'menu_gui.ui']
+        "menu": ['rice_menu.txt', 'noodle_menu.txt', 'menu_gui.ui']
     },
     entry_points={
-        "console_scripts": ['YouSmileIServe = main_service:run_cli',
-                            'menu = menu.menu_cli:run_cli'],
-        "gui_scripts": ['menu-gui=menu:run_gui']
+        "console_scripts": ['menu = menu.menu_cli:run_cli'],
+        "gui_scripts": ['menu-gui=menu.menu_gui:run_gui']
     },
 )
